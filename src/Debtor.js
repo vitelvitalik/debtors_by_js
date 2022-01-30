2a33fc413a872b91dc3d473226c333862045ef99
vitelvitalik <615156@gmail.com>
2022-01-30 20:01:12 +0300

    commit 5
    переделываю классовым способом

--- src/Debtor.js
+++ src/Debtor.js
@@ -1,27 +1,64 @@
-import React, { Component } from 'react'
-import { App } from './App';
+import React, {Component} from 'react'
+import {Debtorlist} from './Debtorlist';
+
+
 
 export class Debtor extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            id: 0,
             name: "",
             summ: "",
-            returned: false
-            }
+            oldSumm: 0,
+            returned: false,
+        };
+
     }
 
-    // deleteList() {
-    //     var result = window.confirm(`Вы действительно хотите удалить всех должников ?`);
-    //     var tmp = result == true? setListDebtors([]) : null;
-    //    };
-    // render() {
-    //     return (
-    //         <div>
-    //             <button onClick={this.deleteList}>Удалить всех</button>
-    //         </div>
-    //     )
-    // }
+    // ввод задолжника
+    doInput = (e) => {
+        console.log(this.state);
+       // если данные вводятся в поле сумма, то дублируем их в oldSumm
+        if ([e.target.name] == 'summ') {
+            this.state[e.target.name] = Number(e.target.value);
+            this.state.oldSumm = Number(e.target.value);
+
+            // иначе просто обновляем debtor
+        } else {
+            this.state[e.target.name] = e.target.value;
+        }
+    };
+
+    add = () => {
+        const list = new Debtorlist();
+        list.addDebtor(this.state)
+    };
+
+
+
+    render() {
+        return (
+        <div>
+            <label> Фамилия И.О. </label>
+            <input
+                name="name"
+                type="text"
+                placeholder="обязательно..."
+                value={this.props.name}
+                onChange={this.doInput}
+            />
+            <label> Сумма </label>
+            <input
+                name="summ"
+                type="number"
+                min="0"
+                value={this.props.summ}
+                placeholder="обязательно..."
+                onChange={this.doInput}
+            />
+            <button onClick={this.add}>Добавить</button>
+        </div>
+        )
+    }
 }
--- src/Debtorlist.js
+++ src/Debtorlist.js
@@ -1,37 +1,187 @@
-import React, { Component } from 'react'
-import { Debtor } from './Debtor';
+import React, {Component} from 'react'
+import {Debtor} from './Debtor';
 
-export default class Debtorlist extends Component {
+export class Debtorlist extends Component {
+    // listDebtors = [];
+
     constructor(props) {
         super(props);
 
-        this.state = [];
+        this.state = {
+            listDebtors : []
+            // listDebtors : [{id:1, total:4, Debtor:{name:"hfgf", summ:55, oldsumm:55, returned: true}}]
+            // listDebtors: function () {
+            //     const readListDebtors = localStorage.getItem("listDebtors");
+            //     console.log("читается масив: ");
+            //     console.log(JSON.parse(readListDebtors));
+            //     return (readListDebtors ? JSON.parse(readListDebtors) : []);
+            // }
+        };
     }
 
-    addDebtor() {
-        // this.setState (, debtor])
+    addDebtor = (Debtor) => {
+        console.log(this.state)
+        if (Debtor.summ == "") {
+            window.alert("Необходимо указать сумму")
+        } else if (Debtor.name == "") {
+            window.alert("Необходимо указать Ф.И.О. должника")
+        } else {
+
+            // устанавливаем переменную итоговой суммы
+            let totalSumm = Number(Debtor.summ);
+
+            // если список не пустой
+            if (this.state.listDebtors.length > 0) {
+
+                // определяем id  последнего элемента
+                const oldId = this.state.listDebtors[this.state.listDebtors.length - 1].id;
+
+                // считаем итоговую сумму всего списка
+                this.listDebtors.forEach(debtors => {
+                    totalSumm += Number(debtors.Debtor.summ)
+                });
+
+                // обнуляем итоговую сумму total у всех элементов
+                this.listDebtors.forEach(debtors => {
+                    debtors.total = 0
+                });
+
+                // устанавливаем новому элементу списка oldId+1, итоговую сумму и обновляем список
+                this.setState({listDebtors:this.listDebtors.push({id: oldId + 1, total: totalSumm, Debtor})});
+
+                // иначе начинаем нумерацию id с 1
+            } else {
+                this.setState({listDebtors: this.listDebtors.push({id: 1, total: totalSumm, Debtor})});
+            }
+
+            // обнулим должника
+            // Debtor({
+            //     name: "",
+            //     summ: "",
+            //     oldSumm: 0,
+            //     returned: false
+            // });
+        }
     }
+
+    deleteDebtor(e) {
+        // определим имя удаляемого должника
+        const delDebtor = this.listDebtors[this.listDebtors.findIndex(item => item.id + '' === e.target.id)].Debtor;
+
+        // запрос на подтверждение удаления
+        const result = window.confirm(`Вы действительно хотите удалить должника "${delDebtor.name}" ?`);
+
+        // устанавливаем переменную итоговой суммы
+        let totalSummDel = 0;
+
+        // если ответ положительный, то удаляем
+        if (result === true) {
+            switch (this.listDebtors.length) {
+                case 1: {
+                    this.listDebtors = [];
+                    break;
+                }
+                default: {
+                    this.listDebtors.splice(this.listDebtors.findIndex(item => item.id + '' === e.target.id), 1);
+
+                    // вычисляем итоговую сумму
+                    this.listDebtors.forEach(debtors => {
+                        totalSummDel += Number(debtors.Debtor.summ)
+                    });
+
+                    // меняем значение итоговой суммы у последнего в списке
+                    this.listDebtors[this.listDebtors.length - 1].total = totalSummDel;
+                    break;
+                }
+            }
+        }
+    }
+
+    doReturn(e) {
+        // определим переменную индекса элемента списка
+        const index = this.listDebtors.findIndex(item => item.id + '' === e.target.id);
+
+        // определим текущее состояние признака возврата
+        const returned = this.listDebtors[index].Debtor.returned;
+
+        // определим сумму задолженности
+        const oldSumm = this.listDebtors[index].Debtor.oldSumm;
+        const summ = this.listDebtors[index].Debtor.summ;
+
+        // инвертируем признак возврата
+        this.listDebtors[index].Debtor.returned = !returned;
+
+        // по признаку возврата изменяем сумму задолженности - 0 либо старая сумма
+        if (returned == false) {
+            this.listDebtors[index].Debtor.summ = 0;
+            this.listDebtors[this.listDebtors.length - 1].total -= summ;
+        } else {
+            this.listDebtors[index].debtor.summ = oldSumm;
+            this.listDebtors[this.listDebtors.length - 1].total += oldSumm;
+        }
+    }
+
+    deleteList() {
+        let result = window.confirm(`Вы действительно хотите удалить всех должников ?`);
+        if (result === true) {
+            this.listDebtors = [];
+        }
+    }
+
+
     render() {
         return (
             <div>
-              <Debtor />
-              {listDebtors.map(function (debtor) {
-                <tr>
-                  <td>{Debtor.name}</td>
-                  <td>{Debtor.summ}</td>
-                  <td>
-                    <input
-                      name="returned"
-                      type="checkbox"
-                      id={Debtor.id}
-                      checked={Debtor.returned}
-                      onChange={} />
-                  </td>
-                  <td>
-                    <button onClick={deleteDebtor}>Удалить</button>
-                  </td>
-                </tr>
-              })}
+                <h2>Список должников</h2>
+
+                <table className='App-table' border="1" cellPadding="7" cellSpacing="0">
+                    <thead>
+                    <tr>
+                        <th>№ п/п</th>
+                        <th width="50%">Фамилия И.О.</th>
+                        <th>Задолженность</th>
+                        <th>Вернул</th>
+                        <th>Удаление</th>
+                    </tr>
+                    </thead>
+                    <tbody >
+                    {this.state.listDebtors.map(function (debtors) {
+                        return (
+                            <tr key={"debtorKey" + debtors.id}>
+                                <td key="debtorId">{(this.listDebtors.findIndex(item => item.id === debtors.id)) + 1}</td>
+                                <td key="{debtorName}" align="left">{debtors.Debtor.name}</td>
+                                <td key="{debtorSumm}">{debtors.Debtor.summ}</td>
+
+                                <td key="{debtorReturned}">
+                                    <input
+                                        key="{debtorReturnedKey}"
+                                        name="returned"
+                                        type="checkbox"
+                                        id={debtors.id}
+                                        checked={debtors.Debtor.returned}
+                                        onChange={this.doReturn}/>
+                                </td>
+                                <td key="{debtorDelete}">
+                                    <button key="{debtorDelKey}" id={debtors.id} onClick={this.deleteDebtor}>Удалить
+                                    </button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                    </tbody>
+                    <tfoot>
+
+                    <tr>
+                        <td></td>
+                        <th>ИТОГО:</th>
+                        <th>{this.state.listDebtors.length > 0 ? Math.round(this.state.listDebtors[this.state.listDebtors.length - 1].total * 100) / 100 : 0}</th>
+                        <td></td>
+                        <td>
+                            <button onClick={this.deleteList}>Удалить всех</button>
+                        </td>
+                    </tr>
+                    </tfoot>
+                </table>
             </div>
         )
     }
